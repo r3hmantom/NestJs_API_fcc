21365b3ff49eb4081abc9ce7d7b37196a805eefb
Abdul Rehman <r3hmantom@gmail.com>
2023-04-02 09:43:25 +0500

    added dto in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post } from '@nestjs/common';
 import { Body, Req } from '@nestjs/common/decorators';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: any) {
+  signup(@Body() dto: AuthDto) {
     console.log({
       dto,
     });
