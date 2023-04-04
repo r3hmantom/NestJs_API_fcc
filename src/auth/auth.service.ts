98ffcc34e34bed53062ccc987ea54c92419cb05c
Abdul Rehman <r3hmantom@gmail.com>
2023-04-04 21:27:27 +0500

    try catch to get specified msg on multi emails

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -14,16 +18,31 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
 
     //save the user in the database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
 
-    delete user.hash;
-    //return the saved user
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      //return the saved user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials Taken',
+          );
+        }
+      }
+      throw error;
+    }
 
     return 'I am sign up';
   }
