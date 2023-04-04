715cbe375d4d95042ef2bd36b05881051838900b
Abdul Rehman <r3hmantom@gmail.com>
2023-04-04 20:59:43 +0500

    added validation + whitelist vulnerability

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // it strips out extra info from the request body, vulnerable bugs
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
