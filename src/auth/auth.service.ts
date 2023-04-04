e4369d3ca119fd638cd25a48f4002bc301d966e8
Abdul Rehman <r3hmantom@gmail.com>
2023-04-04 21:57:55 +0500

    changed signup position, added body dec

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,17 +10,7 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  async signin(dto: AuthDto) {
-    // find the user by email
 
-    // if user does not exist throw exception
-
-    // compare password
-    // if password incorrect throw exception
-
-    // send back the user
-    return 'I am sign in';
-  }
   async signup(dto: AuthDto) {
     //generate the password hash
     const hash = await argon.hash(dto.password);
@@ -53,5 +43,37 @@ export class AuthService {
     }
 
     return 'I am sign up';
+  }
+
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    // if user does not exist throw exception
+    if (!user)
+      throw new ForbiddenException(
+        'Invalid Credentials',
+      );
+
+    // compare password
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    // if password incorrect throw exception
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Crendentials Incorrect',
+      );
+
+    // send back the user
+    delete user.hash;
+    return user;
+    return 'I am sign in';
   }
 }
