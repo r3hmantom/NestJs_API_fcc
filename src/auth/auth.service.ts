81936e4122a1c95c8bd155422c11a2e97c417737
Abdul Rehman <r3hmantom@gmail.com>
2023-04-04 21:02:57 +0500

    passed dto in signup function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -8,7 +8,7 @@ export class AuthService {
   signin() {
     return 'I am sign in';
   }
-  signup() {
+  signup(dto: AuthDto) {
     return 'I am sign up';
   }
 }
