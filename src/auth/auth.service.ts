d98a5b9a59a121eb53974a7d6051dac035708b2f
Abdul Rehman <r3hmantom@gmail.com>
2023-04-04 21:11:42 +0500

    hashed pass, created and returned user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -8,12 +9,20 @@ export class AuthService {
   signin() {
     return 'I am sign in';
   }
-  signup(dto: AuthDto) {
+  async signup(dto: AuthDto) {
     //generate the password hash
+    const hash = await argon.hash(dto.password);
 
     //save the user in the database
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
 
     //return the saved user
+    return user;
 
     return 'I am sign up';
   }
