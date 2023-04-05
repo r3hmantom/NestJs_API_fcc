d8778d91ec3ebec9fb91926d42a2b2334f90558f
Abdul Rehman <r3hmantom@gmail.com>
2023-04-05 15:45:19 +0500

    reverted success

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,12 +12,12 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDto) {
-    return this.authService.signup(dto);
+  signup() {
+    return this.authService.signup();
   }
 
   @Post('signin')
-  signin(@Body() dto: AuthDto) {
-    return this.authService.signin(dto);
+  signin() {
+    return this.authService.signin();
   }
 }
