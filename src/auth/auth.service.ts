1497881f3b5de92f015900203f4b3cd38196d26a
Abdul Rehman <r3hmantom@gmail.com>
2023-04-05 14:46:40 +0500

    config env file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,12 +7,14 @@ import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private prisma: PrismaService,
     private jwt: JwtService,
+    private config: ConfigService,
   ) {}
 
   async signup(dto: AuthDto) {
@@ -29,9 +31,8 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
       //return the saved user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (
         error instanceof
@@ -76,8 +77,21 @@ export class AuthService {
       );
 
     // send back the user
-    delete user.hash;
-    return user;
-    return 'I am sign in';
+    return this.signToken(user.id, user.email);
+  }
+
+  signToken(
+    userId: number,
+    email: string,
+  ): Promise<string> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret,
+    });
   }
 }
