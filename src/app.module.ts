f251da8c60f99480f9c43e01f7994c5b26ce859a
Abdul Rehman <r3hmantom@gmail.com>
2023-04-05 13:51:20 +0500

    db url from env file, set config to global

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({}),
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UserModule,
     BookmarkModule,
